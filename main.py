5a157b6f995b92609e29c2f2c47b8ed7b97bc48c
Bhagat Aastha <109658@eesd.org>
2021-01-12 00:31:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# Variables are containers, hold a value
+# Data Types / Variable Types:
+# String (str): combination of. charecters (letters, numbers, or symbols) - must use quotation marks
+# Integer (int) : whole numbers
+# Character (char) : single letter, number, or symbol
+# Float : numbers with decimals
+# Boolean (bool):  True / False
+
+# Why do we need data types
+# computers are the smartest dumb things in the world
+# 2 + 2 = ?? what do you want the computer to do.
+
+# formula:
+# variableName = value
+# Rules fpr variable names:
+# 1. always start with a letter
+# 2. can only contain numbers and letters
+# 3. if more that one word is used, use camelCase (first word is lowercase and every word after that is uppercase)
+
+firstName = "Aastha" #String type
+
+## FirstName. (ok, but use lower f)
+## 1stname. (bad)
+## name1 (ok)
+
+favoriteBook = "Bedtime Stories" #string
+favNumber = 3 #Integer
+isHungry = True #Boolean
+favChar = 'l' #Char
+pi = 3.14159 #Float
